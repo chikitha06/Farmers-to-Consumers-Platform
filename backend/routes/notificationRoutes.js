5e7c2a9c24f9b0605312644572488b56cc5b58d0
chikitha06 <atavalichikitha06@gmail.com>
2026-09-21 20:39:33 +0530

    fix

--- backend/routes/notificationRoutes.js
+++ backend/routes/notificationRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getMyNotifications, markAsRead, markAllAsRead } = require('../controllers/NotificationController');
+const { getMyNotifications, markAsRead, markAllAsRead } = require('../controllers/notificationController')
 const { protect } = require('../middleware/authMiddleware');
 
 router.get('/', protect, getMyNotifications);
